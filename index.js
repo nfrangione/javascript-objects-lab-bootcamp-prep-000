3ccbcf52a0473a69b46a8ecf41266e0261a84a7c
Nicholas Frangione <nfrangione8@gmail.com>
2021-01-28 04:44:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var recipes = new Object();
 
 function updateWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]:value});
+}
+
+function destructivelyUpdateWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
-
-function 
